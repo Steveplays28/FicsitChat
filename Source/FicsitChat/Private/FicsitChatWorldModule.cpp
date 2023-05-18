96dda28cba905e9b4577df71da81d941e4c927de
Steveplays28 <djspaargaren@outlook.com>
2023-05-18 21:38:03 +0200

    feat: :sparkles: Send Discord messages to Satisfactory chat

--- Source/FicsitChat/Private/FicsitChatWorldModule.cpp
+++ Source/FicsitChat/Private/FicsitChatWorldModule.cpp
@@ -1,5 +1,6 @@
 #include "FicsitChatWorldModule.h"
 #include "Configuration/ConfigManager.h"
+#include "Containers/UnrealString.h"
 
 UFicsitChatWorldModule::UFicsitChatWorldModule() {
 #if !WITH_EDITOR
@@ -9,19 +10,6 @@ UFicsitChatWorldModule::UFicsitChatWorldModule() {
 
 // Runs on game world load
 void UFicsitChatWorldModule::DispatchLifecycleEvent(ELifecyclePhase Phase) {
-	if (Phase == ELifecyclePhase::POST_INITIALIZATION) {
-		FChatMessageStruct chatMessageStruct{};
-		chatMessageStruct.MessageString = TEXT("Hello from FICSIT.chat!");
-		chatMessageStruct.MessageType = EFGChatMessageType::CMT_PlayerMessage;
-		chatMessageStruct.ServerTimeStamp = GetWorld()->TimeSeconds;
-		chatMessageStruct.CachedPlayerName = TEXT("Discord nickname here");
-
-		AFGChatManager *chatManager = AFGChatManager::Get(GetWorld());
-		chatManager->AddChatMessageToReceived(chatMessageStruct);
-
-		UE_LOG(LogFicsitChat, Verbose, TEXT("Sent chat message: %s"), TEXT("Hello from FICSIT.chat!"));
-	}
-
 	if (Phase != ELifecyclePhase::INITIALIZATION)
 		return;
 
@@ -30,21 +18,40 @@ void UFicsitChatWorldModule::DispatchLifecycleEvent(ELifecyclePhase Phase) {
 
 	UE_LOG(LogFicsitChat, Verbose, TEXT("Bot token: %s"), *config.BotToken);
 
-	bot = MakeShared<dpp::cluster>(TCHAR_TO_ANSI(*config.BotToken));
+	// Start Discord bot
+	bot = MakeShared<dpp::cluster>(TCHAR_TO_ANSI(*config.BotToken), dpp::i_default_intents | dpp::i_message_content);
 
-	AsyncThread([=]() {
-		bot->on_slashcommand([](auto event) {
+	bot->on_message_create([&](const dpp::message_create_t &event) {
+		AsyncTask(ENamedThreads::GameThread, [=]() {
+			FString messageContent = event.msg.content.c_str();
+			FString messageAuthor = event.msg.author.format_username().c_str();
+
+			FChatMessageStruct chatMessageStruct{};
+			chatMessageStruct.MessageString = messageContent;
+			chatMessageStruct.MessageType = EFGChatMessageType::CMT_PlayerMessage;
+			chatMessageStruct.ServerTimeStamp = GetWorld()->TimeSeconds;
+			chatMessageStruct.CachedPlayerName = messageAuthor;
+
+			AFGChatManager *chatManager = AFGChatManager::Get(GetWorld());
+			chatManager->AddChatMessageToReceived(chatMessageStruct);
+		});
+	});
+
+	bot->on_slashcommand([](auto event) {
+		AsyncThread([=]() {
 			if (event.command.get_command_name() == "ping") {
 				event.reply("Pong!");
 			}
 		});
+	});
 
-		bot->on_ready([&](auto event) {
+	bot->on_ready([&](auto event) {
+		AsyncThread([=]() {
 			if (dpp::run_once<struct register_bot_commands>()) {
 				bot->global_command_create(dpp::slashcommand("ping", "Ping pong!", bot->me.id));
 			}
 		});
-
-		bot->start(false);
 	});
+
+	AsyncThread([=]() { bot->start(false); });
 }
